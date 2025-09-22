7d767e76ad91b1886e26366bf1317ab3467cf634
Rodev <robertfemu45@gmail.com>
2025-09-22 20:52:19 +0200

    feat: configurar Vite para usar Tailwind CSS y React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +11,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()],
-  output: 'server'
+  integrations: [react()]
 });
